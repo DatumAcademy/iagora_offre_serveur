e2410e666fe08cda91fff91b58a3fa262de91814
Nick Mathieu <rakotomanananick29@gmail.com>
2024-09-08 16:15:04 +0300

    [Serveur] Résoudre conflict

--- routes/OfferRoute.js
+++ routes/OfferRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const offerController = require('../controllers/OfferController');
 
+router.get('/search', offerController.searchOffers);
 router.get('/student/:numETU', offerController.getStudentCandidate);
 router.get('/statistics', offerController.getStatistique);
 router.get('/:id', offerController.getOfferDetails);
-router.get('/search', offerController.searchOffers);
 router.post('/create', offerController.createOffer);
 router.delete('/:id', offerController.deleteOffer);
 
